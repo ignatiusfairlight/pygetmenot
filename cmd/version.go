9bf9a0fad96990b85e6657e36aac85bc2b786446
ignatiusfairlight <lawaierang@gmail.com>
2026-08-07 12:09:16 +0800

    Added version method

--- cmd/version.go
+++ cmd/version.go
@@ -1,1 +1,23 @@
 package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
+var (
+	version = "dev"
+	
+	versionCmd = &cobra.Command{
+	Use: "version",
+	Short: "...",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(version)
+	},
+}
+
+)
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
