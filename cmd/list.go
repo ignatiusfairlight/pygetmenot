5431ea58d50b6c86cf769dbfbb578e22eeda8ab7
ignatiusfairlight <lawaierang@gmail.com>
2026-08-07 11:57:23 +0800

    Scaffolded list.go

--- cmd/list.go
+++ cmd/list.go
@@ -1,1 +1,21 @@
 package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
+var (
+	listCmd = &cobra.Command{
+	Use: "list",
+	Short: "...",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Welcome to Python's list. We are definitely not an array, ahahaha...")
+	},
+}
+
+)
+
+func init() {
+	rootCmd.AddCommand(listCmd)
+}
