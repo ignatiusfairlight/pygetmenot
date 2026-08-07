0f16b4d1ca6995a56c73e1c29d8c3ee893f7802b
ignatiusfairlight <lawaierang@gmail.com>
2026-08-07 11:41:48 +0800

    Fleshed out the scaffold and removed stray file

---
+++ cmd/root.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"github.com/spf13/cobra"
+)
+
+var (
+	rootCmd = &cobra.Command{
+	Use: "pygetmenot",
+	Short: "A terminal app that reminds you how to use Python features.",
+	Long: `A terminal app designed to assist in reminding you how to write Python
+	codes with detailed explanation and code examples enough for you to understand 
+	and continue your work when you are stuck in writing any Python projects.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("I love Ricardo!")
+	},
+}
+
+)
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
