a416b34c2b44a109794486a6ac73058948ba97b2
ignatiusfairlight <lawaierang@gmail.com>
2026-08-07 12:00:39 +0800

    Scaffolded loop and tuple

--- cmd/loop.go
+++ cmd/loop.go
@@ -1,1 +1,21 @@
 package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
+var (
+	loopCmd = &cobra.Command{
+	Use: "loop",
+	Short: "...",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Praise Oroboros!")
+	},
+}
+
+)
+
+func init() {
+	rootCmd.AddCommand(loopCmd)
+}
--- cmd/tuple.go
+++ cmd/tuple.go
@@ -1,1 +1,21 @@
 package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
+var (
+	tupleCmd = &cobra.Command{
+	Use: "tuple",
+	Short: "...",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("You can't change me!")
+	},
+}
+
+)
+
+func init() {
+	rootCmd.AddCommand(tupleCmd)
+}
